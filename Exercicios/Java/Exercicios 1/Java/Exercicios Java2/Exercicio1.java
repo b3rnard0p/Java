ac5881c64b7f937ad9ad3e2aaa251a92a284a641
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-04-01 18:53:58 -0300

    Delete Exercicios directory

--- Exercicios/Java/Exercicios 1/Java/Exercicios Java2/Exercicio1.java
+++
@@ -1,9 +0,0 @@
-package Exercicio1;
-
-public class Exercicio1 {
-    public static void main(String[] args) {
-        for (int i = 1; i < 11; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- Exercicios/Java/Exercicios 1/Java/Exercicios Java2/Exercicio11.java
+++
@@ -1,9 +0,0 @@
-public class Exercicio11 {
-    public static void main(String[] args) {
-        for (int i = 1000; i < 2000; i++) {
-            if (i % 11 == 5) {
-                System.out.println(i);
-            }
-        }
-    }
-}
