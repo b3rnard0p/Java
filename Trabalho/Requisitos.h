ee375c9e825957ebcdbee1d04e4bd495a9bf2064
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-04-22 21:08:21 -0300

    Add files via upload

---
+++ Trabalho/Requisitos.h
@@ -0,0 +1,32 @@
+//Menu
+//1 - Listar atletas(nome ou por pontuação decrescente)
+//2 - Cadastrar atletas
+//3 - Procurar por atletas (nome ou apelido)
+//4 - Remover atleta (nome ou apelido)
+//Opção:
+
+//A classe de dados deve ser:
+
+//Class Atleta {
+    //String fone;
+    //String nome;
+    //String apelido;
+    //String dataNascimento;
+    //int pontuacaoAcumulada;
+//}
+
+//O código deve conter:
+
+//Construtores
+
+//Getters Setters
+
+//ToString
+
+//Equals
+
+//HashCode
+
+//Obs: Todo sistema deve controlar duplicados. A chave de unicidade é o fone.
+
+//Opcional: Guardar a informações em arquivo csv.
