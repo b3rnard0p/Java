8ab22545917dc05ef2c836d954863717024f6013
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-05-14 20:59:04 -0300

    Add files via upload

---
+++ Exercicios/Exercicios 7/src/Exercicio2/FormaGeometrica.java
@@ -0,0 +1,9 @@
+package Exercicio2;
+
+public class FormaGeometrica {
+	
+	public int  calcularArea(int b, int a) {
+		return b*a;
+	}
+
+}
---
+++ Exercicios/Exercicios 7/src/Exercicio3/ContaBancaria.java
@@ -0,0 +1,8 @@
+package Exercicio3;
+
+public class ContaBancaria {
+
+	public double calcularSaldo(double saldo) {
+		return saldo;
+	}
+}
