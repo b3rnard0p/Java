a43334fba3f44609be990322176c125967537807
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-05-17 20:55:33 -0300

    Add files via upload

---
+++ Herança/Exemplo2/src/jogorapido2/PessoaJuridica.java
@@ -0,0 +1,29 @@
+package jogorapido2;
+
+public class PessoaJuridica extends Pessoa{
+	
+	protected String cnpj;
+	protected String socio;
+	protected String dtAbertura;
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getSocio() {
+		return socio;
+	}
+	public void setSocio(String socio) {
+		this.socio = socio;
+	}
+	public String getDtAbertura() {
+		return dtAbertura;
+	}
+	public void setDtAbertura(String dtAbertura) {
+		this.dtAbertura = dtAbertura;
+	}
+	
+	
+}
