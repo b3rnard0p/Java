08ac8327bf54a05255bffe1bd1ca5a19bdf32fae
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-09-12 19:45:32 -0300

    Update Ordenacao.java

--- Estudos/Exemplos/Ordenação/Ordenação/src/Ordenação/Ordenacao.java
+++ Estudos/Exemplos/Ordenação/Ordenação/src/Ordenação/Ordenacao.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 
 public class Ordenacao {
     
-    public static void bolha(ArrayList<Aluno>lista){
+    public static void pente(ArrayList<Aluno>lista){
         Aluno tmp;
         boolean houveTroca;
         int i;
+        int distancia = lista.size();
         
         do {
+            distancia = (int)(distancia / 1.3);
+            if (distancia < 1) distancia = 1;
             houveTroca = false;
-           for(i=0;i<lista.size()-1;i++){
+           for(i=0;i+distancia < lista.size();i++){
                if(lista.get(i).compareTo(lista.get(i+1)) > 0){
                    tmp = lista.get(i);
                    lista.set(i, lista.get(i+1));
@@ -20,7 +23,7 @@ public class Ordenacao {
                }
                
             }
-        }while(houveTroca);
+        }while(distancia > 1 || houveTroca);
     }
        
         
